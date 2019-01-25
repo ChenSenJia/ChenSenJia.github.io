a12354800d0c1db1bf0fe018386934e6abfa61d4
ChenSenJia <c07686734917@gmail.com>
2019-01-25 17:46:20 +0800

    Site updated: 2019-01-25 17:46:04

---
+++ dist/music.js
@@ -0,0 +1,31 @@
+const ap = new APlayer({
+    container: document.getElementById('aplayer'),
+    fixed: true,
+    autoplay: false,
+    audio: [
+      {
+        name: "春夏秋冬",
+        artist: "张国荣",
+        url: 'http://www.ytmp3.cn/down/51185.mp3',
+        cover: 'http://p2.music.126.net/Ix8Kv3SXWCnHMKuG_G633g==/879609302221142.jpg',
+      },
+      {
+        name: "The Beat Goes On",
+        artist: "Beady Eye",
+        url: 'https://link.hhtjim.com/163/16821632.mp3',
+        cover: 'https://p2.music.126.net/C5HHGFUjXLd-j4CpHFxPnA==/18404725137826011.jpg',
+      },
+      {
+        name: "Idlers Dream",
+        artist: "Oasis",
+        url: 'https://link.hhtjim.com/163/4225641.mp3',
+        cover: 'http://p1.music.126.net/PVVIgRuFfwuP0PS0NCblFQ==/1762517139334368.jpg',
+      },
+      {
+        name: "Golden Slumbers (Acoustic)",
+        artist: "Dua Lipa",
+        url: "https://link.hhtjim.com/163/522631901.mp3",
+        cover: "http://p2.music.126.net/l2T6qCPxw4ZkOWu2zAv_yw==/18209012068172109.jpg",
+      }
+    ]
+});
